62ce5de4c8508f9dbd2396a3666d2756da55ac49
michaelsuryadi <michaelsuryadi@yahoo.co.id>
2013-01-17 19:24:51 +0700

    New Netbeans Project

---
+++ src/Driver.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mike
+ */
+public class Driver {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
